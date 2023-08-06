a1e6c50874b7bd38867a8e240535b245d554a39e
SeyidaliAgayev <seyidali.aga@div.edu.az>
2023-08-06 21:43:34 +0400

    Next project STUDENTMS

---
+++ src/model/Student.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Student {
+    private int id;
+    private String fullname;
+    private String group;
+    private String birthdate;
+    private String speciality;
+
+}
+
